2c96e9ccf2b966d7a1cb8a9f214b30f33b43fc2a
Guilherme <112200498+GuilhermeArmstrong@users.noreply.github.com>
2024-10-28 11:28:17 -0300

    até a aula 6

--- graficos/common.js
+++ graficos/common.js
@@ -3,9 +3,29 @@ const getCSS = (variavel) => {
 }
 
 const tickConfig = {
-
-    color: getCSS ('--cor-primaria'),
-    family: getCSS ('--font'),
-    size:18
+    color: getCSS('--cor-primaria'),
+    size: 16,
+    family: getCSS('--font')
 }
-export {getCSS,tickConfig}
+
+function criarGrafico(data, layout) {
+    const grafico = document.createElement('div');
+    grafico.className = 'grafico';
+    document.getElementById('graficos-container').appendChild(grafico);
+    const config = {
+        responsive: true,
+        displayModeBar: false
+    }
+    Plotly.newPlot(grafico, data, layout, config);
+}
+
+
+function incluirTexto(texto) {
+    const container = document.getElementById('graficos-container')
+    const paragrafo = document.createElement('p')
+    paragrafo.classList.add('graficos-container__texto')
+    paragrafo.innerHTML = texto
+    container.appendChild(paragrafo)
+}
+
+export { getCSS, tickConfig, criarGrafico, incluirTexto }
